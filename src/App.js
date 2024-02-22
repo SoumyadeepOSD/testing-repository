f4a86104b9effc4b65adc7f01b0e3551242718b1
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-22 16:15:26 +0530

    Build generated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Latest from './pages/Latest';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src="/static/Images/email_icon_2.png"/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/latest' element={<Latest/>}/>        
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
