c00b7766bb77c1cf1a5529d740cd5c5c2eeaf798
SoumyadeepOSD <opencontainer255@gmail.com>
2024-02-22 16:31:31 +0530

    transformed to basic layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
-import { Route, Routes } from 'react-router-dom';
-import Latest from './pages/Latest';
-import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home.jsx";
+import Latest from "./pages/Latest.jsx";
 
 const App = () => {
   return (
-    <div>
-      <img src="/static/Images/email_icon_2.png"/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/latest' element={<Latest/>}/>        
-      </Routes>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path ="/latest" element={<Latest/>}/>
+    </Routes>
+  );
 }
 
 export default App
